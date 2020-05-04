7914ee6f797bd138b313296cbb0df855785dae1f
ramgmtk <ramgmtk@gmail.com>
2020-05-03 21:00:47 -0700

    Comments for graders.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,38 @@
+/*
+Creative Tilt:
+Programming:
+    The enemy spawn system is a modified version of what Professor Nathan uses
+    in paddle parkour. The tweaks I made where that this delayed event call is 
+    called every 'n' seconds, where the event calls a function. That function then 
+    selects an enemy spawn pattern to bring into the game based off of the current 
+    dicciulty level. The difficulty level is adjusted by another event timer that goes 
+    off every 10 seconds 'k' times. In tandem these two timers work together to dynamically 
+    spawn enemies to the screen for players to avoid during gameplay. The code is not 
+    optimized efficiently, but I thought the approach was modular and interesting.
+    This can be found in game.js under the create() method: the timed events are this.obstacleSpawn,
+    and this.difficultyBump. The enemy spawner function that gets called is generateObstacles().
+
+    An additional note I found cool is how we used a sprite to represent a timer. Phasers 
+    anims class allows the use of setting the duration of a sprite animation. One of the 
+    sprite animations we use represents the cooldown of the players projectile. The cooldown 
+    for that projectile goes down with increases in difficulty. The cool part is that this 
+    gets reflected in the sprites animation as it gets sped up the faster the cooldown is.
+    The code for the animation can be found in game.js under the create() method: 
+    this.anims.create: key 'stand_cooldown'. This may be a brute force method of implementing this
+    feature but I think it worked out fine.
+
+Art: 
+    Our ending screen is reminiscent of snes game over screens. We destroyed all objects on 
+    screen with the exception of the player. The background cuts to black and as they are brought 
+    to the center, then a spotlight shines on them. The game over text then fades in followed by an 
+    ominous background. The cherry on top is a bit cruched sound byte of one of our groupmates taunting 
+    the player. The background art was also drawn by one of our groupmates and greatly adds impact 
+    to the death screen.
+
+*/
+
+
+
 let config = {
     type: Phaser.CANVAS,
     width: 1080,
